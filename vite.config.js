cea6da0c91de16ec067616139ca83fb478a0e379
Julia Shlykova <julia.shlykova17@gmail.com>
2026-04-15 12:37:21 +0700

    change base and homepage

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-page/',
+  // base: '/personal-page/',
   css: {
     preprocessorOptions: {
       scss: {
